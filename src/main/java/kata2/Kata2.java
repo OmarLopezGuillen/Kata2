b6aa55286ed4c63e2c269aa911328605835c4d15
omar <omar@macbook-pro-de-omar.home>
2022-10-11 16:40:21 +0100

    Commit v5

--- src/main/java/kata2/Kata2.java
+++ src/main/java/kata2/Kata2.java
@@ -7,12 +7,12 @@ import java.util.Map;
 public class Kata2{
 
     public static void main(String[] args) {
-       Integer[] data = {1, 2, 1, 3, 3, 4, 5, 2, 1, 1, 1, 6, 7, 8, 8};
+       String[] data = {"Rosa", "Pepe", "María", "Pepe", "Pepe", "Rosa"};
        
        HistogramGenerator histogramGenerator = new HistogramGenerator(data);
-       Map<Integer, Integer> histogram = histogramGenerator.getHistogram();
+       Map<String, Integer> histogram = histogramGenerator.getHistogram();
 
-        for (Map.Entry<Integer, Integer> entry: histogram.entrySet()) {
+        for (Map.Entry<String, Integer> entry: histogram.entrySet()) {
             System.out.println(entry.getKey() + "====>" + entry.getValue());
         }
     }
