1e7024020d8b411d46aa9ca93383013e0ea9fd4a
omar <omar@macbook-pro-de-omar.home>
2022-10-11 16:36:44 +0100

    Commit v4

--- src/main/java/kata2/HistogramGenerator.java
+++ src/main/java/kata2/HistogramGenerator.java
@@ -1,17 +1,18 @@
 package kata2;
 
+
 import java.util.HashMap;
 import java.util.Map;
 
-public class HistogramGenerator {
-    private final int[] data;
+public class HistogramGenerator<T> {
+    private final T[] data;
 
-    public HistogramGenerator(int[] data) {
+    public HistogramGenerator(T[] data) {
         this.data = data;
     }
     
-    Map<Integer, Integer> getHistogram() {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    Map<T, Integer> getHistogram() {
+        Map<T, Integer> map = new HashMap<T, Integer>();
         
         for (int i = 0; i < data.length; i++) {
             map.put(data[i], map.containsKey(data[i]) ? map.get(data[i]) + 1 : 1);
--- src/main/java/kata2/Kata2.java
+++ src/main/java/kata2/Kata2.java
@@ -1,18 +1,19 @@
 package kata2;
 
+
 import java.util.HashMap;
 import java.util.Map;
 
-public class Kata2 {
+public class Kata2{
 
     public static void main(String[] args) {
-       int[] data = {1, 2, 1, 3, 3, 4, 5, 2, 1, 1, 1, 6, 7, 8, 8};
+       Integer[] data = {1, 2, 1, 3, 3, 4, 5, 2, 1, 1, 1, 6, 7, 8, 8};
        
        HistogramGenerator histogramGenerator = new HistogramGenerator(data);
        Map<Integer, Integer> histogram = histogramGenerator.getHistogram();
 
         for (Map.Entry<Integer, Integer> entry: histogram.entrySet()) {
-            System.out.println(entry.getKey() + " ==> " + entry.getValue());
+            System.out.println(entry.getKey() + "====>" + entry.getValue());
         }
     }
 }
